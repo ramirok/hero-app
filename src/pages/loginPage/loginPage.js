2872b6af2c3764c20624c87fb071b81e03bb9813
ramirok <krupoviesaramiro@gmail.com>
2021-10-28 14:30:32 -0300

    fix css logo

--- src/pages/loginPage/loginPage.js
+++ src/pages/loginPage/loginPage.js
@@ -7,8 +7,8 @@ const LoginPage = () => {
   return (
     <div className="container-fluid">
       <div className="row d-flex justify-content-center position-relative">
-        <div className="col col-md-4 align-items-center position-relative d-flex flex-column min-vh-100">
-          <Logo />
+        <div className="col col-xl-4 col-md-6 align-items-center position-relative d-flex flex-column min-vh-100">
+          <Logo style={{ maxHeight: "350px" }} />
           <div className="flex-grow-1 d-flex align-items-center justify-content-center w-100">
             <LoginForm />
           </div>
@@ -16,8 +16,6 @@ const LoginPage = () => {
             Made by Luis Ramiro Krupoviesa {new Date().getFullYear()}
           </div>
         </div>
-
-        {/* <div className="col-8 d-md-block d-none bg-dark">landing content</div> */}
       </div>
     </div>
   );
